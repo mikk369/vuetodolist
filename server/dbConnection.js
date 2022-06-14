e1a4129de9d23f8f9ad9025412c445fb7b422afd
mikk369 <90323811+mikk369@users.noreply.github.com>
2022-06-14 18:35:53 +0300

    Update dbConnection.js

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -4,7 +4,7 @@ const { Users } = require("./models/UsersSchema");
 
 // Connecting to database
 mongoose.connect(
-  "mongodb+srv://test:test@cluster0.rogo5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  "mongoDB URl",
   { useNewUrlParser: true }
 );
 
